c7a840bfc55f0f40e10987ac212a920a97640cbd
mbnq <mbnq00@gmail.com>
2024-08-31 14:46:41 +0200

    reworked sounds again

--- Glass/glassRmbMenu.cs
+++ Glass/glassRmbMenu.cs
@@ -1,54 +1,57 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Drawing.Text;
+using System.Windows.Forms;
 using MaterialSkin.Controls;
 
 namespace RED.mbnq
 {
     public partial class GlassHudOverlay
     {
+        private Action playSND = Sounds.PlayClickSound;     // PlayClickSoundOnce
         private void OverlayForm_MouseClick(object sender, MouseEventArgs e)
         {
             if (e.Button == MouseButtons.Right)
             {
                 ContextMenuStrip menu = new MaterialContextMenuStrip();
-                Sounds.PlayClickSoundOnce();
+                playSND();
 
                 menu.Items.Add((debugInfoDisplay.IsGlassMenuEnabled ? "Close " : "Open ") + "Glass Settings", null, (s, ea) => { 
                     ToggleGlassMenu();
-                    Sounds.PlayClickSoundOnce();
+                    playSND();
                 });  // Open Menu
 
                 menu.Items.Add(new ToolStripSeparator());
                 menu.Items.Add("Change Capture Region", null, async (s, ea) =>
                 {
-                    Sounds.PlayClickSoundOnce();
+                    playSND();
                     await GlassHudOverlay.RestartWithNewAreaAsync();
                 });
 
                 menu.Items.Add(new ToolStripSeparator());
                 menu.Items.Add("Copy to Clipboard", null, (s, ea) => { 
                     mbFnc.CopyOverlayToClipboard(this, GetAdjustedCaptureArea());
-                    Sounds.PlayClickSoundOnce();
+                    playSND();
                 });
 
                 menu.Items.Add(new ToolStripSeparator());
                 menu.Items.Add(isMoveEnabled ? "Bind" : "Unbind", null, (s, ea) => { 
                     ToggleMoveOption();
-                    Sounds.PlayClickSoundOnce();
+                    playSND();
                 });
 
                 menu.Items.Add("Toggle Border", null, (s, ea) => { 
                     ToggleFrameVisibility();
-                    Sounds.PlayClickSoundOnce();
+                    playSND();
                 });
                 menu.Items.Add("Toggle Shape", null, (s, ea) => { 
                     ToggleShape();
-                    Sounds.PlayClickSoundOnce();
+                    playSND();
                 });
 
                 menu.Items.Add(new ToolStripSeparator());
                 menu.Items.Add("Close Glass", null, (s, ea) => { 
                     this.Close();
-                    Sounds.PlayClickSoundOnce();
+                    playSND();
                 });
 
                 menu.Show(this, e.Location);
