7bbac0f27a6d806abaee37490b141fcc35b90c89
mbnq <mbnq00@gmail.com>
2024-09-09 15:11:53 +0200

    rmbMenu slight improvement to make the code more compact and avoid redundancy

--- core/mbnqRmbMenu.cs
+++ core/mbnqRmbMenu.cs
@@ -1,11 +1,4 @@
-﻿/* 
-    www.mbnq.pl 2024 
-    mbnq00 on gmail
-
-    Mouse menu comes here
-*/
-
-using MaterialSkin.Controls;
+﻿using MaterialSkin.Controls;
 using System;
 using System.Diagnostics;
 using System.IO;
@@ -18,217 +11,130 @@ namespace RED.mbnq
     {
         private ControlPanel controlPanel;
         private mbnqConsole textHUD;
-        private ToolStripMenuItem centerMenuItem, saveMenuItem, loadMenuItem, aboutMenuItem, closeMenuItem, loadCustomMenuItem, removeCustomMenuItem, newCaptureRegionMenuItem, textConsoleMenuItem;    // openSettingsDirMenuItem
-        private ToolStripSeparator separator1, separator2, separator3, separator4, separator5, separator6;
+        private ToolStripMenuItem removeCustomMenuItem, loadCustomMenuItem;
+
         public rmbMenu(ControlPanel controlPanel)
         {
             this.controlPanel = controlPanel;
-
-            // Initialize menu item separators
-            separator1 = new ToolStripSeparator();
-            separator2 = new ToolStripSeparator();
-            separator3 = new ToolStripSeparator();
-            separator4 = new ToolStripSeparator();
-            separator5 = new ToolStripSeparator();
-            separator6 = new ToolStripSeparator();
-
-            // New Capture Region Menu Item
-            newCaptureRegionMenuItem = new ToolStripMenuItem("New Glass Element");
-            newCaptureRegionMenuItem.Click += NewCaptureRegionMenuItem_Click;
-
-            // Initialize menu item Browse UserData
-            ToolStripMenuItem openSettingsDirMenuItem = new ToolStripMenuItem("Browse User Data");
-            openSettingsDirMenuItem.Click += OpenSettingsDirMenuItem_Click;
-
-            // Initialize menu item Center Overlay
-            centerMenuItem = new ToolStripMenuItem("Center Overlay");
-            centerMenuItem.Click += centerMenuItem_Click;
-
-            // Text Console HUD
-            textConsoleMenuItem = new ToolStripMenuItem("Show Debug Console");
-            textConsoleMenuItem.Click += TextHUDMenuItem_Click;
-
-            // Initialize menu item Save settings
-            saveMenuItem = new ToolStripMenuItem("Save settings");
-            saveMenuItem.Click += saveMenuItem_Click;
-
-            // Initialize menu item Load settings
-            loadMenuItem = new ToolStripMenuItem("Load settings");
-            loadMenuItem.Click += loadMenuItem_Click;
-
-            // Initialize menu item Close
-            closeMenuItem = new ToolStripMenuItem("Close");
-            closeMenuItem.Click += CloseMenuItem_Click;
-
-            // Initialize menu item About
-            aboutMenuItem = new ToolStripMenuItem("About");
-            aboutMenuItem.Click += AboutMenuItem_Click;
-
-            // Initialize menu item Load Custom
-            loadCustomMenuItem = new ToolStripMenuItem("Load Custom PNG");
-            loadCustomMenuItem.Click += LoadCustomPNG_Click;
-
-            // Initialize menu item Remove Custom
-            removeCustomMenuItem = new ToolStripMenuItem("Remove Custom PNG");
-            removeCustomMenuItem.Click += RemoveCustomMenuItem_Click;
-            removeCustomMenuItem.Enabled = File.Exists(Path.Combine(SaveLoad.SettingsDirectory, "RED.custom.png"));
-
-            /* --- --- --- Menu --- --- --- */
-
-            this.Items.Add(saveMenuItem);
-            this.Items.Add(loadMenuItem);
-            this.Items.Add(separator6);
-            this.Items.Add(openSettingsDirMenuItem);
-            this.Items.Add(separator5);
-            this.Items.Add(loadCustomMenuItem);
-            this.Items.Add(removeCustomMenuItem);
-            this.Items.Add(separator4);
-            this.Items.Add(textConsoleMenuItem);
-            this.Items.Add(separator3);
-            // this.Items.Add(centerMenuItem);
-            this.Items.Add(newCaptureRegionMenuItem);
-            this.Items.Add(separator2);
-            this.Items.Add(aboutMenuItem);
-            this.Items.Add(separator1);
-            this.Items.Add(closeMenuItem);
-
+            InitializeMenuItems();
             UpdateMenuItems();
         }
 
-        /* --- --- --- --- --- --- */
+        private void InitializeMenuItems()
+        {
+            var saveMenuItem = CreateMenuItem("Save settings", saveMenuItem_Click);
+            var loadMenuItem = CreateMenuItem("Load settings", loadMenuItem_Click);
+            var openSettingsDirMenuItem = CreateMenuItem("Browse User Data", OpenSettingsDirMenuItem_Click);
 
-        // open player's data folder
+            loadCustomMenuItem = CreateMenuItem("Load Custom PNG", LoadCustomPNG_Click);
+            removeCustomMenuItem = CreateMenuItem("Remove Custom PNG", RemoveCustomMenuItem_Click);
+            UpdateMenuItems(); // Initial enable/disable logic is handled in UpdateMenuItems
+
+            var textConsoleMenuItem = CreateMenuItem("Show Debug Console", TextHUDMenuItem_Click);
+            var newCaptureRegionMenuItem = CreateMenuItem("New Glass Element", NewCaptureRegionMenuItem_Click);
+            var aboutMenuItem = CreateMenuItem("About", AboutMenuItem_Click);
+            var closeMenuItem = CreateMenuItem("Close", CloseMenuItem_Click);
+
+            this.Items.AddRange(new ToolStripItem[]
+            {
+                saveMenuItem, loadMenuItem, new ToolStripSeparator(),
+                openSettingsDirMenuItem, new ToolStripSeparator(),
+                loadCustomMenuItem, removeCustomMenuItem, new ToolStripSeparator(),
+                textConsoleMenuItem, new ToolStripSeparator(),
+                newCaptureRegionMenuItem, new ToolStripSeparator(),
+                aboutMenuItem, new ToolStripSeparator(),
+                closeMenuItem
+            });
+        }
+
+        private ToolStripMenuItem CreateMenuItem(string text, EventHandler onClick)
+        {
+
+            var menuItem = new ToolStripMenuItem(text);
+            menuItem.Click += (sender, e) =>
+            {
+                Sounds.PlayClickSoundOnce();
+                onClick(sender, e);
+            };
+            return menuItem;
+        }
+
         private void OpenSettingsDirMenuItem_Click(object sender, EventArgs e)
         {
-            Sounds.PlayClickSoundOnce();
             try
             {
                 string settingsDir = SaveLoad.SettingsDirectory;
-
                 if (Directory.Exists(settingsDir))
-                {
-                    System.Diagnostics.Process.Start("explorer.exe", settingsDir);
-                }
+                    Process.Start("explorer.exe", settingsDir);
                 else
-                {
-                    MaterialMessageBox.Show("Settings directory not found.", "Error!", MessageBoxButtons.OK, MessageBoxIcon.None);
-                    Sounds.PlayClickSoundOnce();
-                }
+                    ShowMessageBox("Settings directory not found.", "Error!");
             }
             catch (Exception ex)
             {
-                MaterialMessageBox.Show($"Failed to open settings directory: {ex.Message}", "Error!", MessageBoxButtons.OK, MessageBoxIcon.None);
-                Sounds.PlayClickSoundOnce();
+                ShowMessageBox($"Failed to open settings directory: {ex.Message}", "Error!");
             }
         }
 
-        // text hud
         private void TextHUDMenuItem_Click(object sender, EventArgs e)
         {
-            Sounds.PlayClickSoundOnce();
-
             if (textHUD == null || textHUD.IsDisposed)
-            {
                 textHUD = new mbnqConsole();
-            }
-
             textHUD.ToggleOverlay();
-            textConsoleMenuItem.Text = textHUD.Visible ? "Close Debug Console" : "Show Debug Console"; // rename it to Console Hud?
         }
 
-        // Event handler for the new capture region menu item
         private void NewCaptureRegionMenuItem_Click(object sender, EventArgs e)
         {
-            Sounds.PlayClickSoundOnce();
-
-            // Code to select a new capture area and display the overlay
-            Rectangle captureArea = selector.SelectCaptureArea();
-            GlassHudOverlay.displayOverlay = new GlassHudOverlay(captureArea, captureArea); // Pass the same region for both for now
-            GlassHudOverlay.displayOverlay.Show(); // Show the overlay
+            var captureArea = selector.SelectCaptureArea();
+            GlassHudOverlay.displayOverlay = new GlassHudOverlay(captureArea, captureArea);
+            GlassHudOverlay.displayOverlay.Show();
         }
 
-        // center overlay
-        private void centerMenuItem_Click(object sender, EventArgs e)
-        {
-            Sounds.PlayClickSoundOnce();
-            controlPanel.CenterCrosshairOverlay();
-        }
+        private void saveMenuItem_Click(object sender, EventArgs e) => SaveLoad.SaveSettings(controlPanel, false);
+        private void loadMenuItem_Click(object sender, EventArgs e) => SaveLoad.LoadSettings(controlPanel, false);
+        private void AboutMenuItem_Click(object sender, EventArgs e) => Process.Start(new ProcessStartInfo("https://www.mbnq.pl") { UseShellExecute = true });
+        private void CloseMenuItem_Click(object sender, EventArgs e) => Application.Exit();
 
-        // saveLoad settings
-        private void saveMenuItem_Click(object sender, EventArgs e)
-        {
-            Sounds.PlayClickSoundOnce();
-            SaveLoad.SaveSettings(controlPanel, false);
-        }
-        private void loadMenuItem_Click(object sender, EventArgs e)
-        {
-            Sounds.PlayClickSoundOnce();
-            SaveLoad.LoadSettings(controlPanel, false);
-        }
-
-        // about
-        private void AboutMenuItem_Click(object sender, EventArgs e)
-        {
-            Process.Start(new ProcessStartInfo
-            {
-                FileName = "https://www.mbnq.pl",
-                UseShellExecute = true
-            });
-            Sounds.PlayClickSoundOnce();
-        }
-
-        // close aka exit
-        private void CloseMenuItem_Click(object sender, EventArgs e)
-        {
-            Application.Exit();
-        }
-
-        // load custom .png
         private void LoadCustomPNG_Click(object sender, EventArgs e)
         {
-            Sounds.PlayClickSoundOnce();
             controlPanel.LoadCustomCrosshair();
             controlPanel.mbCrosshairOverlay.SetCustomPNG();
-
-            if (controlPanel.SizeValue < 100) { controlPanel.SizeValue = 100; }
-
+            if (controlPanel.SizeValue < 100) controlPanel.SizeValue = 100;
+            AdjustColorsForCustomPNG();
+            controlPanel.CenterCrosshairOverlay();
+            controlPanel.updateMainCrosshair();
             UpdateMenuItems();
+        }
 
-            // if player set those to 0 to avoid artifacts on custom .png edges make it now visible
+        private void AdjustColorsForCustomPNG()
+        {
             if (controlPanel.colorR.Value > 200 || controlPanel.colorG.Value > 200 || controlPanel.colorB.Value > 200)
             {
                 controlPanel.colorR.Value = 10;
                 controlPanel.colorG.Value = 10;
                 controlPanel.colorB.Value = 10;
             }
-
-            controlPanel.CenterCrosshairOverlay();
-            controlPanel.updateMainCrosshair();
         }
 
-        // remove custom .png
         private void RemoveCustomMenuItem_Click(object sender, EventArgs e)
         {
-            Sounds.PlayClickSoundOnce();
             controlPanel.mbCrosshairOverlay.RemoveCustomCrosshair();
             controlPanel.RemoveCustomCrosshair();
-
-            controlPanel.ColorRValue++; // nasty hack to force redraw of xhair, still... it works
-
-            UpdateMenuItems();
+            controlPanel.ColorRValue++; // Force redraw of crosshair
             controlPanel.updateMainCrosshair();
-            // SaveLoad.LoadSettings(controlPanel, false);
+            UpdateMenuItems();
         }
 
-        /* --- --- ---  --- --- --- */
-        // refresh menu
         private void UpdateMenuItems()
         {
-            // bool hasCustomOverlay = controlPanel.MainDisplay.HasCustomOverlay;
             bool hasCustomOverlay = File.Exists(Path.Combine(SaveLoad.SettingsDirectory, "RED.custom.png"));
 
-            removeCustomMenuItem.Enabled = hasCustomOverlay;
             loadCustomMenuItem.Enabled = !hasCustomOverlay;
+            removeCustomMenuItem.Enabled = hasCustomOverlay;
+        }
+
+        private void ShowMessageBox(string message, string caption)
+        {
+            MaterialMessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.None);
         }
     }
 }
