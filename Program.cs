80bdcf441cb4ac48fd28d1ffc0c4ba56de29168c
mbnq <mbnq00@gmail.com>
2024-09-14 19:09:21 +0200

    alpha 16 start

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ namespace RED.mbnq
     {
         public static mbCrosshair mainCrosshair;
         public static int mbFrameDelay = 16;     // in ms, for glass hud, default 60fps 
-        public static float mbVersion = 0.078f;
+        public static float mbVersion = 0.080f;
 
         [DllImport("user32.dll")]
         static extern bool SetProcessDPIAware();
