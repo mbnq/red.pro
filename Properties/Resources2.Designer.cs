06e14bc3b717a35c8c280b24a38091f7dd3b9b64
mbnq <mbnq00@gmail.com>
2024-09-16 19:13:05 +0200

    working splashscreen with texts and gfx

--- Properties/Resources2.Designer.cs
+++ Properties/Resources2.Designer.cs
@@ -118,5 +118,15 @@ namespace RED.mbnq.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap redProSplash {
+            get {
+                object obj = ResourceManager.GetObject("redProSplash", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
