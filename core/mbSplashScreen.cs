48b84014b4a1e07b5be861aa73d56bb26962ce3f
mbnq <mbnq00@gmail.com>
2024-09-16 18:06:50 +0200

    mbSplashScreen

---
+++ core/mbSplashScreen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RED.mbnq.core
+{
+    public partial class mbSplashScreen : Form
+    {
+        public mbSplashScreen()
+        {
+            InitializeSplashScreen();
+        }
+    }
+}
