31f2f38c5808005003009356a4cba5ba6b2aaf37
mbnq <mbnq00@gmail.com>
2024-09-17 18:12:30 +0200

    aboutForm designer cleanup

---
+++ core/mbAboutForm.Designer.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace RED.mbnq.core
+{
+    partial class mbAboutForm
+    {
+        private System.ComponentModel.IContainer components = null;
+        private void InitializeAboutForm()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mbAboutForm));
+
+            // Initialize PictureBox before BeginInit
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+
+            this.SuspendLayout();
+
+            // Initialize labels using the helper method
+            this.materialLabel1 = mbCreateMaterialLabel(
+                name: "materialLabel1",
+                text: "Red Dot Pro Portable by mbnq.pl",
+                location: new Point(106, 114),
+                tabIndex: 0,
+                highEmphasis: true,
+                useAccent: true
+            );
+
+            this.materialLabel2 = mbCreateMaterialLabel(
+                name: "materialLabel2",
+                text: "Website:",
+                location: new Point(6, 182),
+                tabIndex: 1
+            );
+
+            this.materialLabel3 = mbCreateMaterialLabel(
+                name: "materialLabel3",
+                text: "GitHub:",
+                location: new Point(6, 213),
+                tabIndex: 2
+            );
+
+            this.materialLabel4 = mbCreateMaterialLabel(
+                name: "materialLabel4",
+                text: "eMail:",
+                location: new Point(6, 245),
+                tabIndex: 3
+            );
+
+            this.materialLabel5 = mbCreateMaterialLabel(
+                name: "materialLabel5",
+                text: "https://www.mbnq.pl/",
+                location: new Point(73, 182),
+                tabIndex: 4,
+                isUnderlined: true,
+                highEmphasis: true,
+                clickEvent: this.materialLabel5_Click
+            );
+
+            this.materialLabel6 = mbCreateMaterialLabel(
+                name: "materialLabel6",
+                text: "https://github.com/mbnq/red.pro",
+                location: new Point(73, 213),
+                tabIndex: 5,
+                isUnderlined: true,
+                highEmphasis: true,
+                clickEvent: this.materialLabel6_Click
+            );
+
+            this.materialLabel7 = mbCreateMaterialLabel(
+                name: "materialLabel7",
+                text: "mbnq00@gmail.com",
+                location: new Point(73, 245),
+                tabIndex: 6,
+                highEmphasis: true,
+                clickEvent: mbFnc.mbCopyLabelToClipboard
+            );
+
+            // PictureBox settings
+            this.pictureBox1.Image = global::RED.mbnq.Properties.Resources.redProSplash;
+            this.pictureBox1.Location = new System.Drawing.Point(6, 80);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(94, 93);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+
+            // Version Label
+            this.materialLabel8 = mbCreateMaterialLabel(
+                name: "materialLabel8",
+                text: $"Version: {Program.mbVersion} 2024",
+                location: new Point(6, 290),
+                tabIndex: 8
+            );
+
+            // Form properties
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = false;
+            this.ClientSize = new System.Drawing.Size(326, 272);
+            this.Controls.Add(this.materialLabel8);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.materialLabel7);
+            this.Controls.Add(this.materialLabel6);
+            this.Controls.Add(this.materialLabel5);
+            this.Controls.Add(this.materialLabel4);
+            this.Controls.Add(this.materialLabel3);
+            this.Controls.Add(this.materialLabel2);
+            this.Controls.Add(this.materialLabel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.FormStyle = MaterialSkin.Controls.MaterialForm.FormStyles.ActionBar_48;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.ShowInTaskbar = false;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "mbAboutForm";
+            this.Padding = new System.Windows.Forms.Padding(3);
+            this.Text = "RED.PRO";
+            this.TopMost = true;
+            this.StartPosition = FormStartPosition.Manual;
+            this.Location = new Point(
+                ControlPanel.ActiveForm.Location.X + ControlPanel.mCPWidth + 10,
+                ControlPanel.ActiveForm.Location.Y + (ControlPanel.mCPHeight / 2)
+            );
+
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        // helper method for MaterialLabels 
+        private MaterialSkin.Controls.MaterialLabel mbCreateMaterialLabel(
+            string name,
+            string text,
+            Point location,
+            int tabIndex,
+            FontStyle fontStyle = FontStyle.Regular,
+            bool isUnderlined = false,
+            bool highEmphasis = false,
+            bool useAccent = false,
+            EventHandler clickEvent = null
+        )
+        {
+            var label = new MaterialSkin.Controls.MaterialLabel
+            {
+                AutoSize = true,
+                Depth = 0,
+                Font = new Font("Roboto", 14F, fontStyle, GraphicsUnit.Pixel),
+                Location = location,
+                MouseState = MaterialSkin.MouseState.HOVER,
+                Name = name,
+                Text = text,
+                TabIndex = tabIndex,
+                HighEmphasis = highEmphasis,
+                UseAccent = useAccent
+            };
+
+            if (isUnderlined)
+            {
+                label.Font = new Font(label.Font, label.Font.Style | FontStyle.Underline);
+            }
+
+            if (clickEvent != null)
+            {
+                label.Click += clickEvent;
+            }
+
+            return label;
+        }
+
+        private MaterialSkin.Controls.MaterialLabel materialLabel1;
+        private MaterialSkin.Controls.MaterialLabel materialLabel2;
+        private MaterialSkin.Controls.MaterialLabel materialLabel3;
+        private MaterialSkin.Controls.MaterialLabel materialLabel4;
+        private MaterialSkin.Controls.MaterialLabel materialLabel5;
+        private MaterialSkin.Controls.MaterialLabel materialLabel6;
+        private MaterialSkin.Controls.MaterialLabel materialLabel7;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private MaterialSkin.Controls.MaterialLabel materialLabel8;
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ core/mbSplashScreen.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+/* 
+
+    www.mbnq.pl 2024 
+    https://mbnq.pl/
+    mbnq00 on gmail
+
+*/
+
+using System;
+
+namespace RED.mbnq.core
+{
+    partial class mbSplashScreen
+    {
+        public string BuildDate { get; private set; }
+        private System.Windows.Forms.Label labelVersionInfo, labelVersionInfo2;
+
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        public void InitializeSplashScreen()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mbSplashScreen));
+            this.labelVersionInfo = new System.Windows.Forms.Label();
+            this.labelVersionInfo2 = new System.Windows.Forms.Label();
+
+            this.SuspendLayout();
+            // 
+            // mbSplashScreen
+            // 
+            this.AccessibleDescription = "Splash Screen";
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.BackgroundImage = global::RED.mbnq.Properties.Resources.redProSplash;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(512, 512);
+            this.ControlBox = false;
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.Color.Transparent;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "mbSplashScreen";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "RED.PRO SplashScreen";
+            this.TopMost = true;
+            // this.Click += (sender, e) => { this.Visible = false; };
+            // this.FormClosing += (sender, e) => ControlPanel.ActiveForm.Show();
+            // 
+            // labelVersionInfo
+            // 
+            this.labelVersionInfo.AutoSize = true;
+            this.labelVersionInfo.BackColor = System.Drawing.Color.Transparent;
+            this.labelVersionInfo.ForeColor = System.Drawing.Color.White;
+            this.labelVersionInfo.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelVersionInfo.Location = new System.Drawing.Point((this.Location.X + (this.Size.Width - this.labelVersionInfo.Size.Width) - 80), this.Location.Y + (this.Size.Height - this.labelVersionInfo.Size.Height - 20)); // Adjust this position as needed
+            this.labelVersionInfo.Name = "labelVersionInfo";
+            // this.labelVersionInfo.Size = new System.Drawing.Size(250, 24);
+            this.labelVersionInfo.AutoSize = true;
+            this.labelVersionInfo.TabIndex = 0;
+            this.labelVersionInfo.Text = "www.mbnq.pl 2024";
+            // 
+            // labelVersionInfo2
+            // 
+            this.labelVersionInfo2.AutoSize = true;
+            this.labelVersionInfo2.BackColor = System.Drawing.Color.Transparent;
+            this.labelVersionInfo2.ForeColor = System.Drawing.Color.White;
+            this.labelVersionInfo2.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelVersionInfo2.Location = new System.Drawing.Point(this.Location.X + 20, this.Location.Y + this.labelVersionInfo2.Size.Height); // Adjust this position as needed
+            this.labelVersionInfo2.Name = "labelVersionInfo";
+            // this.labelVersionInfo.Size = new System.Drawing.Size(250, 24);
+            this.labelVersionInfo2.AutoSize = true;
+            this.labelVersionInfo2.TabIndex = 0;
+            this.labelVersionInfo2.Text = $"RED.PRO v.{Program.mbVersion}";
+
+
+            // Add the label to the form
+            this.Controls.Add(this.labelVersionInfo);
+            this.Controls.Add(this.labelVersionInfo2);
+
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+    }
+}
