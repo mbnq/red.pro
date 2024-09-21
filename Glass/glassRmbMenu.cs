5ba8f39a35732328d1e6c594f2943a4249dda515
mbnq <mbnq00@gmail.com>
2024-09-21 20:44:37 +0200

    temporary fix for loading glass settings

--- Glass/glassRmbMenu.cs
+++ Glass/glassRmbMenu.cs
@@ -61,10 +61,14 @@ namespace RED.mbnq
                     SaveLoad.mbSaveGlassSettings(this);
                     playSND();
                 });
+
+                /*
+                // Removed for now since it's bugged and dunno how to fix it yet
                 menu.Items.Add("Load Settings", null, async (s, ea) => {
                     await SaveLoad.mbLoadGlassSettings(this);
                     playSND();
                 });
+                */
 
                 menu.Items.Add(new ToolStripSeparator());
                 menu.Items.Add("Close Glass", null, (s, ea) => { 
